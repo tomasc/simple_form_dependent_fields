91a30e4cf470702eb0d62d1850beba285fb2d1bc
Tomáš Celizna <tomas.celizna@gmail.com>
2018-10-12 17:43:39 +0200

    cleanup config

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   entry: './package/src/index.js',
   output: {
-    library: '@tomasc/simple_form_attachments',
+    library: '@tomasc/simple_form_dependent_fields',
     libraryTarget: 'umd',
     umdNamedDefine: true,
     filename: 'index.js',
@@ -39,13 +39,6 @@ module.exports = {
     })
   ],
   resolve: {
-    extensions: ['.coffee', '.js'],
-    alias: {
-      'load-image': 'blueimp-load-image/js/load-image.js',
-      'load-image-meta': 'blueimp-load-image/js/load-image-meta.js',
-      'load-image-exif': 'blueimp-load-image/js/load-image-exif.js',
-      'canvas-to-blob': 'blueimp-canvas-to-blob/js/canvas-to-blob.js',
-      'jquery-ui/widget': 'blueimp-file-upload/js/vendor/jquery.ui.widget.js'
-    }
+    extensions: ['.coffee', '.js']
   }
 };
