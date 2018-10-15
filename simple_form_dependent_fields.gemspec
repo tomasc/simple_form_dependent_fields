a21d293d9813a8eeaba4d4527eb98fe63fd71da0
asgerb <a@asgerbehnckejacobsen.dk>
2018-10-15 11:39:08 +0200

    Cleanup

--- simple_form_dependent_fields.gemspec
+++ simple_form_dependent_fields.gemspec
@@ -20,8 +20,6 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'simple_form'
   spec.add_dependency 'coffee-rails'
 
-  spec.add_dependency 'jquery-rails'
-
   spec.add_development_dependency 'rails'
   spec.add_development_dependency 'bundler', '~> 1.12'
   spec.add_development_dependency 'capybara', '~> 2.1'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,17 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simple_form_dependent_fields'
 
 require 'bundler/setup'
+require 'mongoid'
 
-ENV["RAILS_ENV"] ||= "test"
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../dummy/config/environment', __FILE__)
 
 require 'rails/test_help'
 
-require 'minitest/spec'
-require 'minitest/rails'
+require 'minitest/around'
 require 'minitest/autorun'
+require 'minitest/rails'
 require 'minitest/reporters'
 require 'minitest/spec'
 
