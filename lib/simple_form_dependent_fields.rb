7ec62bd5bffec785ad95eae014e4a1d0268a2318
asgerb <a@asgerbehnckejacobsen.dk>
2019-10-11 22:06:44 +0200

    Remove sprockets stuff

--- lib/simple_form_dependent_fields.rb
+++ lib/simple_form_dependent_fields.rb
@@ -4,10 +4,3 @@ require 'simple_form_dependent_fields/version'
 
 require 'simple_form_dependent_fields/action_view_extension'
 require 'simple_form_dependent_fields/dependent_fields_builder'
-require 'simple_form_dependent_fields/railtie' if defined?(Rails)
-
-module SimpleFormDependentFields
-  def self.asset_path
-    File.expand_path("../assets/javascripts", __FILE__)
-  end
-end
--- lib/simple_form_dependent_fields/railtie.rb
+++
@@ -1,9 +0,0 @@
-require "rails"
-
-module SimpleFormDependentFields
-  class Railtie < Rails::Railtie
-    initializer "simple_form_dependent_fields.assets" do |app|
-      app.config.assets.paths << SimpleFormDependentFields.asset_path
-    end
-  end
-end
